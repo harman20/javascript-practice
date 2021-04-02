8023de651b760f4171a71cbe6beac6ccd0f6cdd3
Harmanjit Singh <57390891+harman20@users.noreply.github.com>
2021-04-02 21:54:13 +0530

    Add files via upload

---
+++ jv5.js
@@ -0,0 +1,26 @@
+<html>
+<head>
+<script>
+
+let firstArray=[1,2,5];
+let secondArray=[3,4,6];
+
+let thirdArray = [...firstArray, ...secondArray];    
+
+console.log(thirdArray);  //[1,2,5,3,4,6]
+
+thirdArray.sort((a,b )=>{
+    if(a>b) 
+    return 1;
+    else
+    return 0;
+});
+console.log(thirdArray); //[1,2,3,4,5,6]
+
+let middleIndex = ((0+thirdArray.length)/2);
+console.log(middleIndex); //3
+console.log(thirdArray[middleIndex]); //4
+
+</script>
+</head>
+</html>
